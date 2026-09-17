6670b2ace8ce2f96234d09f49e43fe619ef82f50
yeokang12-coder <yeokang12@gmail.com>
2026-09-17 11:09:41 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'dinoclass-v5.8';
+const CACHE_NAME = 'dinoclass-v5.9.2';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
-  './css/style.css?v=5.8',
+  './css/style.css?v=5.9.2',
   './js/app.js',
   './js/sound.js',
   './js/storage.js',
